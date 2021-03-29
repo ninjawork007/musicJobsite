37405a0f6d723383331f8108393dfbee10aabc30
Your Name <bouhadjarmedamine@gmail.com>
2021-03-29 23:33:18 +0200

    Correct composer.json file + minor fixes (config & file import)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,8 +3,6 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-require_once __DIR__ . '/../vendor/google/apiclient/vendor/autoload.php';
-
 class AppKernel extends Kernel
 {
     public function registerBundles()
